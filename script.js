cbd66e76f265d68eb8c210b89fa3dafb345f697f
g-gales <graysongales15@gmail.com>
2024-01-01 19:54:26 -0500

    added off-click function to the hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Add a click event listener to the document
+    document.addEventListener('click', function (event) {
+      var menu = document.querySelector('.menu');
+      var menuBtn = document.querySelector('.menu-btn');
+  
+      // Check if the click target is not within the menu or the menu button
+      if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
+        menu.style.display = 'none';
+        menu.style.left = '-250px';
+      }
+    });
+  });
+
 function toggleMenu() {
     var menu = document.querySelector('.menu');
     if (menu.style.display === 'flex') {
