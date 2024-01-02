6d2c5bcf949fee54d934fef0416dce3fc004f8ac
g-gales <graysongales15@gmail.com>
2024-01-01 20:26:55 -0500

    fixed a bug with menu items display

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Add a click event listener to the document
-    document.addEventListener('click', function (event) {
-      var menu = document.querySelector('.menu');
-      var menuBtn = document.querySelector('.menu-btn');
+    var menu = document.querySelector('.menu');
+    var menuBtn = document.querySelector('.menu-btn');
   
-      // Check if the click target is not within the menu or the menu button
+    document.addEventListener('click', function (event) {
       if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
-        menu.style.display = 'none';
-        menu.style.left = '-250px';
+        menu.classList.remove('open');
       }
     });
   });
-
-function toggleMenu() {
+  
+  function toggleMenu() {
     var menu = document.querySelector('.menu');
-    if (menu.style.display === 'flex') {
-      menu.style.display = 'none';
-      menu.style.left = '-250px';
-    } else {
-      menu.style.display = 'flex';
-      menu.style.left = '0';
-    }
+    menu.classList.toggle('open');
   }
